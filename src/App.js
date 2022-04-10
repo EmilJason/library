01bba05835edd99fd381bf56a2913149b291a3bd
EmilJason <emiljasondatuin@gmail.com>
2022-04-10 16:13:45 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {Text} from '@chakra-ui/react'
+import Navbar from "./components/Navbar"
+function App() {
+  return (
+   <>
+    <Navbar />
+    <Text>Hello World</Text>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+  return(
+   <>
+   <div className="navbar">
+    <h3>Library</h3>
+   </div>
+
+   
+   </>
+  );
+} 
